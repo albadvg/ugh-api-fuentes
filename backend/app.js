182833b876412b0c23712328102e9d76a2ac7620
Alba <albadvg@yahoo.es>
2024-01-15 12:23:13 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,79 @@
+/*PAQUETES EXTERNOS*/
+const express = require('express');
+const dotenv = require('dotenv').config();
+const mongoSanitize = require('express-mongo-sanitize');
+const cors = require('cors');
+const logger = require('morgan');
+
+/*MÓDULOS PROPIOS*/
+const HTTPSTATUSCODE = require('./src/utils/httpStatusCode');
+const connectMongo = require('./src/utils/db');
+/*rutas*/
+const fontsRouter = require('./src/routes/fontRoutes');
+const usersRouter = require('./src/routes/userRoutes');
+const projectsRouter = require('./src/routes/projectRoutes');
+
+/*TRAER VARIABLES SENSIBLES*/
+//carga las variables de .env en la variable global process.env de modo que están ocultas pero disponibles en todo el programa
+const PORT = process.env.PORT;
+
+/*CONECTAR CON BASE DE DATOS*/
+connectMongo();
+
+/*INICIAR SERVIDOR*/
+const app = express();
+app.use(express.json()); //interpretar peticiones en json
+app.use(express.urlencoded({extended: true}));//codificar urls en base al estandar
+
+/*SANEAR PETICIONES*/
+mongoSanitize();
+
+/*CONFIGURACIÓN ENCABEZADOS CORS*/
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Acess-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+app.use(cors({
+    origin: ['http://localhost:5000', 'http://localhost:4200', 'http://127.0.0.1:5500'],
+    credencials: true
+}))
+
+/*loggear por consola info de la request con el formato predefinido "dev"*/
+app.use(logger('dev'));
+/*desactivar el encabezado http x-powered-by, que muestra la tecnología con que se desarrolló la api*/
+app.disable('x-powered-by');
+/*esto crea una variable llamada secretKey, con un valor nodeRestApi, pero no se para qué*/
+/*creo que es una clave que se usa para firmar y verificar el jwt*/
+app.set('secretKey', 'nodeRestApi');
+
+/*RUTAS*/
+app.use('/api/fuentes', fontsRouter);
+app.use('/api/usuario', usersRouter);
+app.use('/api/proyectos', projectsRouter);
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Estás en la ruta base'
+    })
+})
+
+/*MANEJO DE ERRORES*/
+app.use((req, res, next) => {
+    let error = new Error();
+    error.status = 404;
+    error.message = HTTPSTATUSCODE[404];
+    next(error);
+})
+app.use((error, req, res, next) => {
+    return res.status(error.status || 500).json(error.message || 'Unexpected error');
+})
+
+/*ESCUCHAR PUERTO*/
+app.listen(PORT, () => {
+    console.log(`servidor corriendo en el puerto ${PORT}`);
+})
+
+
+
+
