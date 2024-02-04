ff41820b5f624bc8cbd939a7d0a0c9630a19d1be
Alba <albadvg@yahoo.es>
2024-02-04 21:06:20 +0100

    cors

--- app.js
+++ app.js
@@ -35,11 +35,8 @@ app.use((req, res, next) => {
     res.header('Acess-Control-Allow-Headers', 'Content-Type');
     next();
 })
-// app.use(cors({
-//     origin: ['http://localhost:5000', 'http://localhost:4200', 'http://127.0.0.1:5500'],
-//     credencials: true
-// }))
-app.use(cors);
+
+app.use(cors());
 
 /*loggear por consola info de la request con el formato predefinido "dev"*/
 app.use(logger('dev'));
